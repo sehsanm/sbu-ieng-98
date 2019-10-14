f9446c5c3856eec32b895479628507de63478175
Ehsan <sehsanm@gmail.com>
2019-10-14 14:16:05 +0330

    Added Rendering

--- web01/index.js
+++ web01/index.js
@@ -1,15 +1,30 @@
 const express = require('express')
+const whiskers = require('whiskers') 
+
 const app = express()
 const port = 3000
 
 console.log('--------------------------- NEW APP ')
 app.use('/' , (req, res, next)=>{
     console.log('Middleware Called!')
+    next();
 });
 
 app.get('/', (req, res) => {
-    console.log('Get method called')    
-    res.send('Hello World!')
+    console.log(req.query)
+    
+
+    res.send( whiskers.render(`
+        <html>
+        <body>
+        <h1>Hello team a new rendering engine is out!</h1>
+        <ul>
+            <li>Foo is:{query.foo}</li>
+            <li>Bar is:{query.bar}</li>
+        <ul>
+        </body>
+        </html>
+    `, req))
 });
 
 
