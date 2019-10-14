70ea17716dea46bc1c7e5ed146d9836d2dbf8962
Ehsan <sehsanm@gmail.com>
2019-10-14 13:15:00 +0330

    Simple middleware added

--- web01/index.js
+++ web01/index.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 3000
 
 console.log('--------------------------- NEW APP ')
+app.use('/' , (req, res, next)=>{
+    console.log('Middleware Called!')
+});
 
 app.get('/', (req, res) => {
     console.log('Get method called')    
     res.send('Hello World!')
-})
+});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
